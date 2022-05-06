251d2c63856b549da07bb551f2b50b038f249325
du11jk <825174096@qq.com>
2022-05-06 16:10:17 +0800

    fix($update): 修复了初次加载数据不更新的问题

---
+++ src/components/UpdateData.vue
@@ -0,0 +1,17 @@
+<template>
+  <div />
+</template>
+
+<script>
+import bus from "vue3-eventbus";
+export default {
+  name: "UpdateData",
+  mounted() {
+    bus.emit("step_update");
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
